fb95384f0691db53edb298f6ad95e814dfc3c277
TriForMine <quentin@triformine.dev>
2024-06-05 12:19:50 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(mut stream) => {
+            Ok((stream, _)) => {
                 println!("accepted new connection");
 
-                let mut buffer = [0; 1024];
+                tokio::spawn(async move {
+                    let mut stream = stream;
+                    let mut buffer = [0; 1024];
 
-                loop {
-                    let bytes_read = stream.read(&mut buffer).unwrap();
-                    if bytes_read == 0 {
-                        println!("connection closed");
-                        break;
+                    loop {
+                        let bytes_read = stream.read(&mut buffer).await.unwrap();
+                        if bytes_read == 0 {
+                            println!("connection closed");
+                            break;
+                        }
+
+                        stream.write(b"+PONG\r\n").await.unwrap();
                     }
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {:?}", e);
             }
         }
     }
