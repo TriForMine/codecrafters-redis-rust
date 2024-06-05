e5e85bb0e62b825053f720b30be984d69ae0549f
TriForMine <quentin@triformine.dev>
2024-06-05 11:24:38 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,16 @@ fn main() {
                 println!("accepted new connection");
 
                 let mut buffer = [0; 1024];
-                stream.read(&mut buffer).unwrap();
 
-                stream.write(b"+PONG\r\n").unwrap();
+                loop {
+                    let bytes_read = stream.read(&mut buffer).unwrap();
+                    if bytes_read == 0 {
+                        println!("connection closed");
+                        break;
+                    }
+
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
